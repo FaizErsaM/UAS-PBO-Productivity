237f902246a56f21558b6ec5d0aa548473d72816
lazur <lazadaeventpocket1@gmail.com>
2026-06-15 23:36:30 +0700

    Delete backend directory

--- backend/src/main/java/com/productivity/backend/analytics/AnalyticsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.productivity.backend.analytics;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AnalyticsRepository extends JpaRepository<AnalyticsInsightEntity, Long> {
-}
